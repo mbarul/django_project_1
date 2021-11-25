205c8c1e321e74e5da9cd893a273e7c650f9065b
mbarul <marek.barul28@gmail.com>
2021-11-25 15:56:46 +0100

    Working with templates

--- project_django/task/todolist_app/views.py
+++ project_django/task/todolist_app/views.py
@@ -1,3 +1,21 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def todolist(request):
+    context = {
+        "welcome_text":"Welcome Todo List Page!",
+    }
+    return render(request, 'todolist.html', context)
+
+def contact(request):
+    context = {
+        "contact_text":"Welcome Contact Page!",
+    }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        "about_text":"Welcome About Page!",
+    }
+    return render(request, 'about.html', context)
